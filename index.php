d64867f574d0c6ace39b2ac85a6112433f0545b0
Avrahman <wiehmanne@hotmail.com>
2020-04-28 12:49:22 +0200

    blog repository created
    Creation if 'index' & 'gitignore' files;
    Creation of 'images' and 'css' directories

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="description" content="Blog de fin de stage Programmeur PHP-Symfony">
+		<meta name="author" content="Abderahmane REDOUANE">
+		<link rel="icon" href="images/icon.gif">
+		<title> Index </title>
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+
+	<body>
+        <div class="corps">
+            <header>
+				<div id="entete_id">
+
+					<div id="titre">
+						<p id="titre1"> B L O G </p>
+					</div>
+				</div>
+				<nav>
+					<ul>
+						<li> Accueil </li>
+						<li> S'inscrire </li>
+						<li> Contact </li>
+					</ul>
+				</nav>
+            </header>
+
+            <section>
+
+                <aside id="aside_id">
+                </aside>
+
+                <article id="article_id">
+                </article>
+
+            </section>
+
+            <footer>
+				<nav>
+					<ul>
+						<li> <a href="#"> Accueil </a> </li>
+						<li> <a href="inscription/inscription.html"> S'inscrire </a> </li>
+						<li> <a href="contact/contact.html"> Contact </a> </li>
+					</ul>
+				</nav>
+            </footer>
+
+        </div>  <!--  class corps -->
+	</body>
+</html>
